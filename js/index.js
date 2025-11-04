f2f34c267c3eada42a1fa9bd3fa4501694393179
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-04 14:38:19 +0530

    new contents added Super-mario-game

--- js/index.js
+++ js/index.js
@@ -1,38 +1,37 @@
-let game=document.getElementById("game")
-let play=document.getElementById("play")
+let game = document.getElementById("game");
+let play = document.getElementById("play");
 
-let x=100
-let y=100
-let jump=20;
+let x = 100;
+let y = 100;
+let jump = 20;
 
+function movePlay(direction) {
+  let width = game.offsetWidth;
+  let height = game.offsetHeight;
+  let playsize = play.offsetWidth;
 
-function movePlay()
-{
-    let width=game.offsetWidth();
-    let height=game.offsetHeight();
-    let playsize=play.offsetWidth();
+  if (direction === "up" && y + jump + playsize <= height) {
+    y += jump;
+  } 
+  else if (direction === "down" && y - jump >= 0) {
+    y -= jump;
+  } 
+  else if (direction === "left" && x - jump >= 0) {
+    x -= jump;
+  } 
+  else if (direction === "right" && x + jump + playsize <= width) {
+    x += jump;
+  }
 
-    if(direction==="up" && y+jump+playsize <= game)
-    {
-        y+=jump
-    }
-    else if(direction==="down" && y-jump >= 0)
-    {
-        y-=jump
-    }
-    else if (direction==="left" && x-jump >= 0)
-    {
-        x-=jump
-    }
-    else if(direction==="right" && x+jump+playsize <= game)
-    {
-        x+=jump
-    }
+  updatePosition();
 }
 
-
-function updatePosition()
-{
-    play.style.left=`${x}px`
-    play.style.bottom=`${y}px`
+function updatePosition() {
+  play.style.left = `${x}px`;
+  play.style.bottom = `${y}px`;
 }
+
+document.getElementById("up").addEventListener("click", () => movePlay("up"));
+document.getElementById("down").addEventListener("click", () => movePlay("down"));
+document.getElementById("left").addEventListener("click", () => movePlay("left"));
+document.getElementById("right").addEventListener("click", () => movePlay("right"));
