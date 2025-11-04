d40686dcaaa6a0c5e0dc712a8831690d09c9a409
Anjana-20782 <anjanasivadasan0305@gmail.com>
2025-11-04 14:20:48 +0530

    new contents added Super-mario-game

---
+++ js/index.js
@@ -0,0 +1,31 @@
+let game=document.getElementById("game")
+let play=document.getElementById("play")
+
+let x=100
+let y=100
+let jump=20;
+
+
+function movePlay()
+{
+    let width=game.offsetWidth();
+    let height=game.offsetHeight();
+    let playsize=play.offsetWidth();
+
+    if(direction==="up" && y+jump+playsize <= game)
+    {
+        y+=jump
+    }
+    else if(direction==="down" && y-jump >= 0)
+    {
+        y-=jump
+    }
+    else if (direction==="left" && x-jump >= 0)
+    {
+        x-=jump
+    }
+    else if(direction==="right" && x+jump+playsize <= game)
+    {
+        x+=jump
+    }
+}
